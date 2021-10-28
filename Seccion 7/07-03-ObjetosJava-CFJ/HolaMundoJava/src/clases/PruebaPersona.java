3377f732eef98e85663031f06e2271aea79aec51
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-28 16:39:31 -0500

    Seccion 7

---
+++ Seccion 7/07-03-ObjetosJava-CFJ/HolaMundoJava/src/clases/PruebaPersona.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class PruebaPersona {
+	public static void main(String[] args) {
+		Persona persona1 = new Persona();
+		persona1.nombre = "Roger";
+		persona1.apellido = "Valencia";
+		persona1.desplegarInformacion();
+	}
+}
