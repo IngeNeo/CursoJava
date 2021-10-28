1fd5e1c92e974dc37c33696a64d9507f32abf620
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-28 16:33:56 -0500

    Seccion 5

---
+++ Seccion 5/05-04-SentenciaSwitch-CFJ/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,26 @@
+//Mi clase en Java
+public class HolaMundo {
+
+	public static void main(String args[]) {
+		var numero = 3;
+		var numeroTexto = "Valor desconocido";
+
+		switch (numero) {
+		case 1:
+			numeroTexto = "Numero uno";
+			break;
+		case 2:
+			numeroTexto = "Numero dos";
+			break;
+		case 3:
+			numeroTexto = "Numero tres";
+			break;
+		case 4:
+			numeroTexto = "Numero cuatro";
+			break;
+		default:
+			numeroTexto = "Caso no encontrado";
+		}
+		System.out.println("numeroTexto = " + numeroTexto);
+	}
+}
